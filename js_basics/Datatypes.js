07103dfac8bf4a558c333ce0a061f15205545144
CodingArunava <129621495+CodingArunava@users.noreply.github.com>
2026-05-22 17:15:38 +0000

    new

---
+++ js_basics/Datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; //treat all js code as newer version
+console.log(3 + 3)
+// code redablity should be high
+
+// number => 2 to the power of 53
+// string=> ""
+// boolean => true or false
+// null => standalone value
+// undefined => no value
+
+console.log(typeof undefined)
+console.log(typeof null)
