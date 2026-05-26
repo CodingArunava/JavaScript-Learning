dc1cd1c84539cf40779377f10ce80b710923a91d
CodingArunava <129621495+CodingArunava@users.noreply.github.com>
2026-05-26 15:37:35 +0000

    objects commit

---
+++ js_basics/objects.js
@@ -0,0 +1,14 @@
+// singleton
+// object leterals
+//  Object.create // it is constructor method creation
+const jsUser = {
+    name: "arunava",
+    age: 20,
+    location: "kolkata",
+    email: "arunava@gmail.com"
+
+}
+
+console.log(jsUser.email); // it is a way to access an objects
+console.log(jsUser["email"]);// it is a another way to access an objects
+
