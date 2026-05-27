187efe2ed358e34ff0003f2f6e50786d5e3a2839
CodingArunava <129621495+CodingArunava@users.noreply.github.com>
2026-05-27 13:29:13 +0000

    new object commit

--- js_basics/objects.js
+++ js_basics/objects.js
@@ -1,8 +1,11 @@
-// singleton
+// const tinderUser = new Object() // singleton
 // object leterals
 //  Object.create // it is constructor method creation
+const mySym = Symbol("keys1")
+
 const jsUser = {
     name: "arunava",
+    [mySym]: "mykeys1",
     age: 20,
     location: "kolkata",
     email: "arunava@gmail.com"
@@ -11,4 +14,16 @@ const jsUser = {
 
 console.log(jsUser.email); // it is a way to access an objects
 console.log(jsUser["email"]);// it is a another way to access an objects
+console.log(typeof jsUser[mySym]);
+console.log(jsUser);
+Object.freeze(jsUser)
+console.log(jsUser);
+jsUser.email = "arunava@yahoo.com"
+console.log(jsUser);
+
+
+
+
+
+
 
