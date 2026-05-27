b53f471f711419afc478611d89c5bdf59d776abf
CodingArunava <129621495+CodingArunava@users.noreply.github.com>
2026-05-27 15:25:09 +0000

    new commit

---
+++ js_basics/objects2.js
@@ -0,0 +1,13 @@
+const myObj = {
+    email: "arunava@gmail.com",
+    fullname:{
+        username:{
+            firstname: "arunava",
+            lastname: "choudhury"
+        }
+    }
+}
+
+console.log(myObj.fullname.username.firstname);
+// console.log(myObj);
+
